c494b1da986c1633c6986fc2b0e4e7ab5b3b87f7
Vitor <castanheira@discente.ufg.br>
2024-07-23 18:02:25 -0300

    initial commit

---
+++ api/src/models/Cart.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const Schema = new mongoose.Schema({
+    products: [{
+        type: mongoose.Schema.Types.ObjectId,         //Array de objetos
+        ref: 'Product'
+    }],
+
+    username: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    address: {
+        street: {
+            type: String,
+            required: true
+        },
+        number: {
+            type: String,
+            required: true
+        },
+
+        city: {
+            type: String,
+            required: true
+        }
+    },
+
+    payment: {
+        card: {
+            number: {
+                type: String
+            },
+            cvc: {
+                type: String
+            }
+        }
+    }
+})
+
+module.exports = mongoose.model('Cart', Schema)
