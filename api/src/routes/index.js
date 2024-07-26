52b768f13a3864368e17201c5c2d58e69705f7e9
felipeabdiel <felipe_brito@discente.ufg.br>
2024-07-26 08:09:08 -0300

    Atualização de gitignore e inclusão de arquivo de definição de rotas.

---
+++ api/src/routes/index.js
@@ -0,0 +1,35 @@
+const { Router } = require('express')
+
+const UserController = require('../controllers/UserController')
+const SessionController = require('../controllers/Login')
+const ProductController = require('../controllers/ProductController')
+const CartController = require('../controllers/CartController')
+const User = require('../models/User')
+
+
+const routes =  Router()
+
+
+routes.post('/users', UserController.createUser)
+routes.get('/users', UserController.getUsers)
+routes.get('/users/:user_id', UserController.getUserById)
+routes.delete('/users/:user_id', UserController.deleteUser)
+routes.patch('/users/:user_id', UserController.updateUser)
+
+routes.post('/sessions', SessionController.createSession)
+
+routes.post('/products/:user_id', ProductController.createProduct)
+routes.get('/:user_id/products', ProductController.getUserProducts)
+routes.patch('/products/:user_id/:product_id', ProductController.updateProduct)
+routes.delete('/products/:user_id/:product_id', ProductController.deleteProduct)
+
+routes.get('/products', ProductController.getProducts)
+routes.get('/products/:product_id', ProductController.getProductById)
+
+routes.post('/carts/:user_id', CartController.createCart)
+routes.get('/carts/:user_id', CartController.getUserCarts)
+
+routes.get('/carts/:user_id/:cart_id', CartController.getCart)
+
+
+module.exports = routes
